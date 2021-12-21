6d62a256ef45ffba6b04f95df8d79e9b1ecec2ed
vandung <DungBVTH2108020@fpt.edu.vn>
2021-12-21 21:43:46 +0700

    sumo

---
+++ SQL4.sql
@@ -0,0 +1,142 @@
+﻿--Tạo CSDL tên là Example4 (lưu ý đường dẫn WHERE phải tồn tại từ trước nếu không sẽ phát sinh lỗi)
+CREATE DATABASE Example4
+ON PRIMARY
+(
+	NAME='Example4',
+	FILENAME='WHERE\Example4.mdf'
+)
+LOG On
+(
+	NAME='Example4',
+	FILENAME='WHERE\Example4.ldf'
+)
+GO
+
+CREATE DATABASE Example4
+ON PRIMARY
+(	NAME = N'Customer_DB',
+	FILENAME = N'C:\Temp\Example4.mdf')
+LOG ON
+(	NAME = N'Customer_DB_log',
+	FILENAME = N'C:\Temp\Example4_DB_log.mdf')
+COLLATE SQL_Latin1_General_CPI_CI_AS
+GO
+
+USE Example4
+GO
+--Thay đổi tên CSDL Example4 thành Example4Test:
+ALTER DATABASE Example4 MODIFY NAME = Example4Test
+GO
+
+USE Example4Test
+GO
+EXECUTE sp_changedbowner @loginame='na'
+EXEC sp_changedbowner
+GO
+
+USE Example4Test;
+GO
+ALTER DATABASE CUST_DB SET AUTO_SHRINK ON
+GO
+
+USE master
+GO
+--Nếu tồn tại đối tượng (CSDL) tên là Example4 thì xáo đối tượng (CSDL) này đi
+IF(DB_ID('Example4') IS NOT NULL)
+	DROP DATABASE Example4
+GO
+--Tạo CSDL tên là Example4 với FileGroup
+CREATE DATABASE Example4
+ON PRIMARY
+	( NAME='Example4_Primary',
+	  FILENAME='C:\Temp\Example4.mdf',
+	  SIZE=4MB,
+	  MAXSIZE=10MB,
+	FILEGROWTH=1MB),
+DILEGROUP Example4_FGI
+	( NAME = 'Example4_FG1_Dat1',
+	  FILENAME='C:\Temp\Example4_FG1_1.ndf',
+	  SIZE=4MB,
+	  MAXSIZE=10MB,
+	  FILEGROWTH=1MB),
+	  ( NAME = 'Example4_FG1_Dat2',
+	  FILENAME='C:\Temp\Example4_FG1_2.ndf',
+	  SIZE=4MB,
+	  MAXSIZE=10MB,
+	  FILEGROWTH=1MB),
+LOG ON
+	( NAME='Example4_log',
+	  FILENAME='C:\Temp\Example4.mdf',
+	  SIZE=4MB,
+	  MAXSIZE=10MB,
+	FILEGROWTH=1MB),
+GO
+
+--Thêm FileGroup vào trong CSDL
+USE master
+GO
+ALTER DATABASE Example4
+ADD FILEGROUP Test1FG1;
+GO
+ALTER DATABASE Example4
+ADD FILE
+(
+	NAME = test1dat3,
+	FILENAME = 'C:\Temp\tidat3.ndf',
+	SIZE = 5MB,
+	MAXSIZE = 100MB,
+	FILEGROWTH = 5MB
+),
+(
+	NAME = test1dat4,
+	FILENAME = 'C:\Temp\tidat4.ndf',
+	SIZE = 5MB,
+	MAXSIZE = 100MB,
+	FILEGROWTH = 5MB
+)
+TO FILEGROUP Test1FG1;
+GO
+
+--Thêm FileGroup vào log file
+USE master;
+GO
+ALTER DATABASE Example4
+ADD LOG FILE 
+(
+	NAME = testlog2,
+	FILENAME = 'C:\Temp\test2log.ldf',
+	SIZE = 5MB,
+	MAXSIZE = 100MB,
+	FILEGROWTH = 5MB
+),
+(
+	NAME = testlog3,
+	FILENAME = 'C:\Temp\test3log.ldf',
+	SIZE = 5MB,
+	MAXSIZE = 100MB,
+	FILEGROWTH = 5MB
+);
+
+--Nếu tồn tại đối tượng (CSDL) tên là Example4 thì xóa đối tượng (CSDL) này đi
+IF (DB_ID('Example4') IS NOT NULL)
+	DROP DATABASE Example4
+GO
+--hoặc sử dụng khung nhìn hệ thống sau:
+IF EXISTS (SELECT * FROM sys.databases WHERE name='Example4')
+   DROP DATABASE Example4
+GO
+
+--Tạo CSDL bản chụp: Chỉ chạy được trên bản Enterprise.
+CREATE DATABASE Example4_Snapshot
+AS SNAPSHOT OF Example4
+
+CREATE DATABASE database_name 
+[ ON
+[ PRIMARY ] [ <filespec> [,...n]]
+[ , <filegroup> [,...n] ]
+[ LOF ON { <filespec> [ ,...n] } ]
+]
+[ COLLATE collation_name ]
+]
+[;]
+
---
+++ SQL6.sql
@@ -0,0 +1,112 @@
+﻿BIF EXISTS (SELECT * FROM sys.databases WHERE Name LIKE 'Example5')
+   DROP DATABASE Example5
+GO
+ECREATE DATABASE Example5
+GO
+QUSE Example5
+GO
+--Tạo bảng Lớp học
+ECREATE TABLE LopHoc (
+   MaLopHoc INT PRIMARY KEY IDENTITY,
+   TenLopHoc VARCHAR (10)
+   (
+GO
+
+--Tạo bảng Sinh viên có khóa ngoại là cột MalopHoc, nối với bảng LopHoc
+CREATE TABLE SinhVien(
+   Maşv int PRIMARY KEY,
+   TenSV varchar(40),
+   MalopHoc int,
+   CONSTRAINT fk FOREIGN KEY (MaLopHoc) REFERENCES LopHoc (MaLopHoc)
+GO
+
+--Tạo bảng SanPham với một cột NULL, một cột NOT NULL
+CREATE TABLE SanPham (
+   MASP int NOT NULL,
+   TénP varchar(40) NULL
+GO
+
+--Tạo bảng với thuộc tính default cho cột Price
+CREATE TABLE StoreProduct(
+   ProductID int NOT NULL,
+   Name varchar(40) NOT NULL,
+   Price money NOT NULL DEFAULT (100)
+GO
+)
+
+-- Thử kiểm tra xem giá trị default có được sử dụng hay không
+INSERT INTO StoreProduct (ProductID, Name) VALUES (111, 'Rivets')
+GO
+SELECT * FROM StoreProduct
+GO
+
+--Tạo bảng ContactPhone với thuộc tính IDENTITY
+CREATE TABLE ContactPhone (
+   Person_ID int IDENTITY(500,1) NOT NULL,
+   MobileNumber bigint NOT NULL
+)
+INSERT INTO ContactPhone (MobileNumber) VALUES (987123454
+SELECT * FROM ContactPhone
+GO
+ 
+ --Tạo cột nhận dạng duy nhất tống thế
+|CREATE TABLE CellularPhone(
+   Person_ID uniqueidentifier DEFAULT NEWID() NOT NULL,
+   PersonName varchar(60) NOT NULL
+)
+
+--Chèn một record vào
+INSERT INTO CellularPhone (PersonName) VALUES ( 'William Smith')
+GO
+
+--Kiểm tra giá trị của cột Person_ID tự động sinh
+SELECT * FROM CellularPhone
+GO
+
+--Tạo bảng ContactPhone với cột MobileNumber có thuộc tính UNIQUE
+CREATE TABLE ContactPhone
+   Person_ID int PRIMARY KEY,
+   MobileNumber bigint UNIQUE,
+   ServiceProvider varchar(30),
+   LandlineNumber bigint UNIQUE
+)
+--Chèn một record vào
+INSERT INTO CellularPhone (PersonName) VALUES('William Smith')
+
+GO
+
+--Chèn 2 bản ghi có giá trị giống nhau ở cột MobileNumber và LanlieNumber
+INSERT INTO ContactPhone1 values (101, 983345674, 'Hutch', NULL)
+INSERT INTO ContactPhone1 values (102, 983345674, 'Alex', 12)
+GO
+
+--Tạo bảng PhoneExpenses có một CHECT ở cột Amount
+CREATE TABLE PhoneExpenses (
+   Expense_ID int PRIMARY KEY,
+   MobileNumber bigint FOREIGN KEY REFERENCES ContactPhone (MobileNumber)
+   Amount bigint CHECK (Amount >0)
+GO
+
+--Chỉnh sửa cột trong bảng
+DALTER TABLE ContactPhone
+   ALTER COLUMN ServiceProvider varchar(45)
+GO
+
+--Xóa cột trong báng
+BALTER TABLE ContactPhone
+    DROP COLUMN ServiceProvider
+GO
+
+--- Them một ràng buộc vào bảng
+DALTER TABLE ContactPhone
+   ADD CONSTRAINT CHK_RC CHECK (RentalCharges >0)
+  I
+GO
+
+--Xóa một ràng buộc
+DALTER TABLE Person.ContactPhone
+   DROP CONSTRAINT CHK_RC
+
+
+
+
